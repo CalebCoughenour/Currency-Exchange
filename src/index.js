4ee287de52a47f317fdde1fb7bd4b33a16a1bc8c
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-04-22 10:15:08 -0700

    add two functions that clear input when submitted and hides display if app is ran after error

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyExchange from './js/currency-api.js';
 
-// function clearText() {
+const clearDisplay = () => {
+  $('#base-currency').text("");
+  $('#converted-currency').text("");
+  $('#conversion-rate').text("");
+  $('#converted-amount').text("");
+};
 
-// }
+const clearInput = () => {
+  $('#main-currency').val("");
+  $('#secondary-currency').val("");
+  $('#currency-amount').val("");
+};
 
 $('#exchange-form').submit(function(e) {
   e.preventDefault();
@@ -16,13 +25,19 @@ $('#exchange-form').submit(function(e) {
   let promise = CurrencyExchange.getRate(mainCurrency, secondaryCurrency, amount);
   promise.then(function(response) {
     const body = JSON.parse(response);
-    console.log(body);
+    clearInput();
     $('#base-currency').text(`Base Currency: ${body.base_code}`);
+    $('#converted-currency').text(`Converted To: ${secondaryCurrency}`);
     $('#conversion-rate').text(`Conversion Rate: ${body.conversion_rate}`);
     $('#converted-amount').text(`Converted Amount: ${body.conversion_result}`);
+    $('#error-display').text("");
   }, function(error) {
+      clearInput();
+      clearDisplay();
       if (error.includes("unsupported-code")) {
         $('#error-display').text(`Unsupported Currency. Please try again.`);
+      } else if (error.includes("quota-reached")) {
+        $('#error-display').text(`We're sorry! Total monthly API limit reached!`);
       }
     }
   );
