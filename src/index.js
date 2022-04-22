ef1e017e6cbdae6b9023d42b9fc23eac257aae6b
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-04-22 09:02:50 -0700

    call the API from the UI

--- src/index.js
+++ src/index.js
@@ -2,3 +2,15 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyExchange from './js/currency-api.js';
+
+$('#exchange-form').submit(function(e) {
+  e.preventDefault();
+  let mainCurrency = $('#main-currency').val();
+  let  secondaryCurrency = $('#secondary-currency').val();
+  let promise = CurrencyExchange.getRate(mainCurrency, secondaryCurrency);
+  promise.then(function(response) {
+    const body = JSON.parse(response)
+    console.log(body);
+  });
+});
