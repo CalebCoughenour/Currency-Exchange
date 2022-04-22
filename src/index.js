7a73eff9e963a5ae1fb8592030fbf218a79517fc
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-04-22 16:21:00 -0700

    properly display whichever input is display either crypto or fiat

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ const clearInput = () => {
   $('#main-currency').val("");
   $('#secondary-currency').val("");
   $('#currency-amount').val("");
+  $('#coin-search').val("");
 };
 
 $('#exchange-form').submit(function(e) {
@@ -34,9 +35,11 @@ $('#exchange-form').submit(function(e) {
     $('#converted-amount').text(`Converted Amount: ${body.conversion_result}`);
     $('.card-user-conversions').show();
     $('#error-display').text("");
+    $('.card-crypto').hide();
   }, function(error) {
       clearInput();
       clearDisplay();
+      $('.card-crypto').hide();
       if (error.includes("unsupported-code")) {
         $('.card-user-conversions').show();
         $('#error-display').text(`Unsupported Currency. Please try again with a valid currency code.`);
@@ -54,22 +57,26 @@ $('#coin-search-button').click(function(e) {
   let promise = CryptoSearch.getCoins(ticker);
   promise.then(function(response) {
   const body = JSON.parse(response);
-  $('.card-results').show();
+  $('.card-crypto').show();
+  $('.card-error').hide();
   $('#coin-name').text(`${body[0].name}`);
   $('#coin-price').text(`Current Price: $ ${parseFloat(body[0].price).toFixed(2)}`);
   $('#price-change').text(`1d Price Change: $ ${parseFloat(body[0]["1d"].price_change).toFixed(2)}`);
   $('#volume').text(`1d Volume: $ ${body[0]["1d"].volume}`);
   $('#market-cap').text(`Market Cap: $ ${body[0].market_cap}`);
-  $('.card-error').hide();
-  $('.card-homer').hide(); 
-  });
+  clearInput();
+  $('.card-user-conversions').hide();
+  }, function(error) {
+    $('.card-crypto').hide();
+    $('show-errors').text(`There was an error processing your request: ${error}`);
+    }
+  );
 });
 
 $(document).ready(function() {
   let promise = CurrencyConversions.getConversions();
   promise.then(function(response) {
     let body = JSON.parse(response);
-    console.log(body);
     $('#conversions-display').text(`USD Conversion Rates - EUR: ${body.conversion_rates.EUR} JPY: ${body.conversion_rates.JPY} GBP: ${body.conversion_rates.GBP} AUD: ${body.conversion_rates.AUD} CAD: ${body.conversion_rates.CAD}`);
     }, function(error) {
       if (error.includes("quota-reached")) {
